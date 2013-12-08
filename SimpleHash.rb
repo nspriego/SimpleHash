c08435eaf4c74d052857e723a52eb4a52dceb2bb
nspriego <nspriego@gmail.com>
2013-12-07 23:24:18 -0700

    Added method for getting load factor

--- SimpleHash.rb
+++ SimpleHash.rb
@@ -1,12 +1,15 @@
 class SimpleHash
 	def initialize
-		@table = []
+		@entries = 0
+		@size = 100
+		@table = Array.new(@size)
 	end
 
 	def store(key, value)
 		key_index = hash_key(key)
 		if @table[key_index].nil?
-			@table[key_index] = value 
+			@table[key_index] = value
+			@entries += 1 
 		else
 			p "Collision detected for #{key}"
 		end
@@ -18,6 +21,10 @@ class SimpleHash
 
 	def print_hash_table
 		@table.each {|i| p i}
+	end
+
+	def load_factor
+		@entries/@size.to_f
 	end
 
 	private
--- driver.rb
+++ driver.rb
@@ -5,7 +5,7 @@ num = 35
 
 num.times.each do |i|
 	hash.store("key#{i}".to_sym, "value#{i}")
-	p hash["key#{i}".to_sym]
 end
 
 hash.print_hash_table
+p hash.load_factor
